333c713513623349bcd2be2f8c7faad2d22a8330
ahmetkaankara <ahmetkaankara1907@gmail.com>
2022-07-05 15:54:46 +0300

    Initial Commit

---
+++ SwiftUI4LockScreenWidgetsTest/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI4LockScreenWidgetsTest
+//
+//  Created by Ahmet Kaan Kara on 5.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI4LockScreenWidgetsTest/SwiftUI4LockScreenWidgetsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI4LockScreenWidgetsTestApp.swift
+//  SwiftUI4LockScreenWidgetsTest
+//
+//  Created by Ahmet Kaan Kara on 5.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI4LockScreenWidgetsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
